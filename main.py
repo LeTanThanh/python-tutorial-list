a60694466f80b7d0280a09a5987636ac01c69aae
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-28 17:24:09 +0700

    What is a List

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
 if __name__ == "__main__":
-  print("Python List")
+  # What is a List
+
+  empty_list = []
+  print(empty_list)
+
+  todo_list = ["Learn Python List", "How to manage List elements"]
+  print(todo_list)
+
+  numbers = [1, 3, 2, 7, 9, 4]
+  print(numbers)
+
+  colors = ["red", "green", "blue"]
+  print(colors)
+
+  coordinates = [[0, 0], [100, 100], [200, 200]]
+  print(coordinates)
