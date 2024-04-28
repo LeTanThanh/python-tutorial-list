f3278056069b968f153d865232cb0fbe8da72119
TanThanhLe <tanthanhcr95@gmail.com>
2024-04-28 17:24:29 +0700

    Accessing elements in a list

--- main.py
+++ main.py
@@ -15,3 +15,16 @@ if __name__ == "__main__":
 
   coordinates = [[0, 0], [100, 100], [200, 200]]
   print(coordinates)
+
+  # Accessing elements in a list
+
+  """
+  list[index]
+  """
+
+  numbers = [1, 3, 2, 7, 9, 4]
+  print(numbers)
+  print(numbers[0])
+  print(numbers[-1])
+  print(numbers[-1])
+  print(numbers[-2])
